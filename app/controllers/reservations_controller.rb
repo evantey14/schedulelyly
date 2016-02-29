7de849d080ae39f7eb091b15d26ca37d9620646c
Evan Tey <evantey14@gmail.com>
2016-02-29 17:28:50 -0500

    new reservation form

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,5 +2,18 @@ class ReservationsController < ApplicationController
   def index
     @reservations = Reservation.all
   end
-
+  def new
+  end
+  def create
+    @new_reservation = Reservation.new(reservation_params)
+    if @new_reservation.save
+      redirect_to :action => 'index'
+    else 
+      redirect_to :action => 'new'
+    end 
+  end
+  private
+    def reservation_params
+      params.require(:reservation).permit(:exam_id, :client_first_name, :client_last_name, :datetime)
+    end
 end
